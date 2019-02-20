836a48f19b84c35f7d1379a2cb5724b8c13e5819
unknown <PCHLEBU@CE10335.corp.capgemini.com>
2019-02-20 22:41:28 +0100

    final

--- src/main/java/com/capgemini/demo/controller/CustomerCtrl.java
+++ src/main/java/com/capgemini/demo/controller/CustomerCtrl.java
@@ -3,10 +3,6 @@ package com.capgemini.demo.controller;
 import com.capgemini.demo.dao.CustomerRepo;
 import com.capgemini.demo.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
